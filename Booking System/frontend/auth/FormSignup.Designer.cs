a22b9affc40542b2532d82f750688dc5c0bb36af
Jamie Stivala <jamiestivala@gmail.com>
2022-07-14 08:08:05 +0200

    Added frontend/auth which has signin and signup

---
+++ Booking System/frontend/auth/FormSignup.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Booking_System.frontend.auth
+{
+    partial class Signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signupLabel = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelIdCard = new System.Windows.Forms.Label();
+            this.textBoxIdCard = new System.Windows.Forms.TextBox();
+            this.labelContactNumber = new System.Windows.Forms.Label();
+            this.textBoxContactNumber = new System.Windows.Forms.TextBox();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.textBoxGender = new System.Windows.Forms.TextBox();
+            this.labelNationality = new System.Windows.Forms.Label();
+            this.textBoxNation = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // signupLabel
+            // 
+            this.signupLabel.AutoSize = true;
+            this.signupLabel.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.signupLabel.Location = new System.Drawing.Point(46, 29);
+            this.signupLabel.Name = "signupLabel";
+            this.signupLabel.Size = new System.Drawing.Size(148, 54);
+            this.signupLabel.TabIndex = 8;
+            this.signupLabel.Text = "Signup";
+            this.signupLabel.Click += new System.EventHandler(this.loginLabel_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(86, 126);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(191, 23);
+            this.textBoxName.TabIndex = 9;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(86, 108);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(39, 15);
+            this.labelName.TabIndex = 10;
+            this.labelName.Text = "Name";
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(86, 172);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(54, 15);
+            this.labelSurname.TabIndex = 12;
+            this.labelSurname.Text = "Surname";
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(86, 190);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(191, 23);
+            this.textBoxSurname.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Email Address";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 254);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 23);
+            this.textBox1.TabIndex = 15;
+            // 
+            // labelIdCard
+            // 
+            this.labelIdCard.AutoSize = true;
+            this.labelIdCard.Location = new System.Drawing.Point(399, 108);
+            this.labelIdCard.Name = "labelIdCard";
+            this.labelIdCard.Size = new System.Drawing.Size(143, 15);
+            this.labelIdCard.TabIndex = 14;
+            this.labelIdCard.Text = "ID Card/Passport Number";
+            // 
+            // textBoxIdCard
+            // 
+            this.textBoxIdCard.Location = new System.Drawing.Point(399, 126);
+            this.textBoxIdCard.Name = "textBoxIdCard";
+            this.textBoxIdCard.Size = new System.Drawing.Size(191, 23);
+            this.textBoxIdCard.TabIndex = 13;
+            // 
+            // labelContactNumber
+            // 
+            this.labelContactNumber.AutoSize = true;
+            this.labelContactNumber.Location = new System.Drawing.Point(86, 363);
+            this.labelContactNumber.Name = "labelContactNumber";
+            this.labelContactNumber.Size = new System.Drawing.Size(96, 15);
+            this.labelContactNumber.TabIndex = 18;
+            this.labelContactNumber.Text = "Contact Number";
+            // 
+            // textBoxContactNumber
+            // 
+            this.textBoxContactNumber.Location = new System.Drawing.Point(86, 381);
+            this.textBoxContactNumber.Name = "textBoxContactNumber";
+            this.textBoxContactNumber.Size = new System.Drawing.Size(191, 23);
+            this.textBoxContactNumber.TabIndex = 17;
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Location = new System.Drawing.Point(399, 172);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(45, 15);
+            this.labelGender.TabIndex = 26;
+            this.labelGender.Text = "Gender";
+            // 
+            // textBoxGender
+            // 
+            this.textBoxGender.Location = new System.Drawing.Point(399, 190);
+            this.textBoxGender.Name = "textBoxGender";
+            this.textBoxGender.Size = new System.Drawing.Size(191, 23);
+            this.textBoxGender.TabIndex = 25;
+            // 
+            // labelNationality
+            // 
+            this.labelNationality.AutoSize = true;
+            this.labelNationality.Location = new System.Drawing.Point(399, 236);
+            this.labelNationality.Name = "labelNationality";
+            this.labelNationality.Size = new System.Drawing.Size(65, 15);
+            this.labelNationality.TabIndex = 24;
+            this.labelNationality.Text = "Nationality";
+            // 
+            // textBoxNation
+            // 
+            this.textBoxNation.Location = new System.Drawing.Point(399, 254);
+            this.textBoxNation.Name = "textBoxNation";
+            this.textBoxNation.Size = new System.Drawing.Size(191, 23);
+            this.textBoxNation.TabIndex = 23;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(399, 300);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(49, 15);
+            this.labelAddress.TabIndex = 20;
+            this.labelAddress.Text = "Address";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(399, 318);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(191, 60);
+            this.textBoxAddress.TabIndex = 19;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(86, 300);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(57, 15);
+            this.labelPassword.TabIndex = 28;
+            this.labelPassword.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(86, 318);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(191, 23);
+            this.textBoxPassword.TabIndex = 27;
+            // 
+            // signupButton
+            // 
+            this.signupButton.Location = new System.Drawing.Point(685, 401);
+            this.signupButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(136, 40);
+            this.signupButton.TabIndex = 30;
+            this.signupButton.Text = "Signup";
+            this.signupButton.UseVisualStyleBackColor = true;
+            // 
+            // Signup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 452);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.textBoxGender);
+            this.Controls.Add(this.labelNationality);
+            this.Controls.Add(this.textBoxNation);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.labelContactNumber);
+            this.Controls.Add(this.textBoxContactNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelIdCard);
+            this.Controls.Add(this.textBoxIdCard);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.signupLabel);
+            this.Name = "Signup";
+            this.Text = "FormSignup";
+            this.Load += new System.EventHandler(this.FormSignup_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label signupLabel;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelIdCard;
+        private System.Windows.Forms.TextBox textBoxIdCard;
+        private System.Windows.Forms.Label labelContactNumber;
+        private System.Windows.Forms.TextBox textBoxContactNumber;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.TextBox textBoxGender;
+        private System.Windows.Forms.Label labelNationality;
+        private System.Windows.Forms.TextBox textBoxNation;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button signupButton;
+    }
+}
---
+++ Booking System/frontend/auth/FormSignup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Booking_System.frontend.auth
+{
+    public partial class Signup : Form
+    {
+        public Signup()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSignup_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
