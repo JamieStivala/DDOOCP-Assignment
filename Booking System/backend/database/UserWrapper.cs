cfa3f777cf047c136b18baba4d1628d274dc56da
Jamie Stivala <jamiestivala@gmail.com>
2022-07-16 14:23:40 +0200

    Started working on a very simple user wrapper

---
+++ Booking System/backend/database/UserWrapper.cs
@@ -0,0 +1,18 @@
+﻿using Booking_System.backend.model.user;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booking_System.backend.database
+{
+    public class UserWrapper
+    {
+        public static User GetUser(string email, string password)
+        {
+            User user = new User("Jamie", "Stivala", email, password, false, new DateTime(), "77223883", "0368201L");
+
+            if (user.ConfirmPassword(password)) return user;
+            else throw new Exception("Invalid password provided for user with this email address.");
+        }
+    }
+}
