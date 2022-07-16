71394d07acfa21ed9cf4bd3f136c0a3d887a1ed6
Jamie Stivala <jamiestivala@gmail.com>
2022-07-16 17:25:11 +0200

    Added CreateUser, meant to create the User in the database

--- Booking System/backend/database/UserWrapper.cs
+++ Booking System/backend/database/UserWrapper.cs
@@ -1,12 +1,33 @@
 ﻿using Booking_System.backend.model.user;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace Booking_System.backend.database
 {
     public class UserWrapper
     {
+
+        public static string CreateUser(User user)
+        {
+
+            string insertQuery = $"INSERT INTO tblUser VALUES" +
+                $"('{user.FirstName}', '{user.LastName}', '{user.Email}', '{user.Password}', '{user.DateOfBirth.ToShortDateString()}', '{user.PhoneNumber}', '{user.IdCard}')"; //Query to insert the information into the database
+
+
+            DatabaseResult result = DatabaseWrapper.InsertIntoDatabase(insertQuery); //Run the query and get the result
+
+            //Switch the result based on the ENUM representing result
+            switch (result)
+            {
+                case DatabaseResult.OK:
+                    return "User created successfully.";
+                case DatabaseResult.DUPLICATE:
+                    return "A user with that email already exists.  Please use another email or contact one of the admins to update your password.";
+                default:
+                    return "An unknown error has occurred.";
+            }
+        }
+
         public static User GetUser(string email, string password)
         {
             User user = new User("Jamie", "Stivala", email, password, false, new DateTime(), "77223883", "0368201L");
