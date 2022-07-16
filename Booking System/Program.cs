2c53dd79e15947796bb8d8ce0c6de219d2d3b05d
Jamie Stivala <jamiestivala@gmail.com>
2022-07-16 14:58:43 +0200

    Converted to NET Framework Project and added User Dataset

--- Booking System/Program.cs
+++ Booking System/Program.cs
@@ -14,7 +14,6 @@ namespace Booking_System
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormSignup());
