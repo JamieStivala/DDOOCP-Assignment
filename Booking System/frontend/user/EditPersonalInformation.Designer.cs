316de9b4b119904d5c0a9f3ed1310a6ccbd022da
Jamie Stivala <jamiestivala@gmail.com>
2022-07-17 12:10:42 +0200

    Edit Personal Information window to update user information

---
+++ Booking System/frontend/user/EditPersonalInformation.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Booking_System.frontend.user
+{
+    partial class EditPersonalInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateInformationLabel = new System.Windows.Forms.Label();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerDOB = new System.Windows.Forms.DateTimePicker();
+            this.labelDateOfBirth = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.labelNationality = new System.Windows.Forms.Label();
+            this.textBoxNation = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.labelContactNumber = new System.Windows.Forms.Label();
+            this.textBoxContactNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelIdCard = new System.Windows.Forms.Label();
+            this.textBoxIdCard = new System.Windows.Forms.TextBox();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // updateInformationLabel
+            // 
+            this.updateInformationLabel.AutoSize = true;
+            this.updateInformationLabel.Font = new System.Drawing.Font("Segoe UI", 30F);
+            this.updateInformationLabel.Location = new System.Drawing.Point(43, 26);
+            this.updateInformationLabel.Name = "updateInformationLabel";
+            this.updateInformationLabel.Size = new System.Drawing.Size(532, 54);
+            this.updateInformationLabel.TabIndex = 9;
+            this.updateInformationLabel.Text = "Update Personal Information";
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.comboBoxGender.Location = new System.Drawing.Point(351, 225);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(164, 21);
+            this.comboBoxGender.TabIndex = 54;
+            // 
+            // dateTimePickerDOB
+            // 
+            this.dateTimePickerDOB.Location = new System.Drawing.Point(351, 115);
+            this.dateTimePickerDOB.Name = "dateTimePickerDOB";
+            this.dateTimePickerDOB.Size = new System.Drawing.Size(164, 20);
+            this.dateTimePickerDOB.TabIndex = 53;
+            // 
+            // labelDateOfBirth
+            // 
+            this.labelDateOfBirth.AutoSize = true;
+            this.labelDateOfBirth.Location = new System.Drawing.Point(351, 99);
+            this.labelDateOfBirth.Name = "labelDateOfBirth";
+            this.labelDateOfBirth.Size = new System.Drawing.Size(66, 13);
+            this.labelDateOfBirth.TabIndex = 52;
+            this.labelDateOfBirth.Text = "Date of Birth";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(76, 266);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 51;
+            this.labelPassword.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(76, 282);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(164, 20);
+            this.textBoxPassword.TabIndex = 50;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Location = new System.Drawing.Point(351, 211);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(42, 13);
+            this.labelGender.TabIndex = 49;
+            this.labelGender.Text = "Gender";
+            // 
+            // labelNationality
+            // 
+            this.labelNationality.AutoSize = true;
+            this.labelNationality.Location = new System.Drawing.Point(351, 267);
+            this.labelNationality.Name = "labelNationality";
+            this.labelNationality.Size = new System.Drawing.Size(56, 13);
+            this.labelNationality.TabIndex = 48;
+            this.labelNationality.Text = "Nationality";
+            // 
+            // textBoxNation
+            // 
+            this.textBoxNation.Location = new System.Drawing.Point(351, 282);
+            this.textBoxNation.Name = "textBoxNation";
+            this.textBoxNation.Size = new System.Drawing.Size(164, 20);
+            this.textBoxNation.TabIndex = 47;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(351, 322);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(45, 13);
+            this.labelAddress.TabIndex = 46;
+            this.labelAddress.Text = "Address";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(351, 338);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(164, 53);
+            this.textBoxAddress.TabIndex = 45;
+            // 
+            // labelContactNumber
+            // 
+            this.labelContactNumber.AutoSize = true;
+            this.labelContactNumber.Location = new System.Drawing.Point(76, 321);
+            this.labelContactNumber.Name = "labelContactNumber";
+            this.labelContactNumber.Size = new System.Drawing.Size(84, 13);
+            this.labelContactNumber.TabIndex = 44;
+            this.labelContactNumber.Text = "Contact Number";
+            // 
+            // textBoxContactNumber
+            // 
+            this.textBoxContactNumber.Location = new System.Drawing.Point(76, 336);
+            this.textBoxContactNumber.Name = "textBoxContactNumber";
+            this.textBoxContactNumber.Size = new System.Drawing.Size(164, 20);
+            this.textBoxContactNumber.TabIndex = 43;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Email Address";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(76, 226);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(164, 20);
+            this.textBoxEmail.TabIndex = 41;
+            // 
+            // labelIdCard
+            // 
+            this.labelIdCard.AutoSize = true;
+            this.labelIdCard.Location = new System.Drawing.Point(351, 156);
+            this.labelIdCard.Name = "labelIdCard";
+            this.labelIdCard.Size = new System.Drawing.Size(129, 13);
+            this.labelIdCard.TabIndex = 40;
+            this.labelIdCard.Text = "ID Card/Passport Number";
+            // 
+            // textBoxIdCard
+            // 
+            this.textBoxIdCard.Location = new System.Drawing.Point(351, 171);
+            this.textBoxIdCard.Name = "textBoxIdCard";
+            this.textBoxIdCard.Size = new System.Drawing.Size(164, 20);
+            this.textBoxIdCard.TabIndex = 39;
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(76, 155);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(49, 13);
+            this.labelSurname.TabIndex = 38;
+            this.labelSurname.Text = "Surname";
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(76, 171);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(164, 20);
+            this.textBoxSurname.TabIndex = 37;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(76, 100);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 36;
+            this.labelName.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(76, 115);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(164, 20);
+            this.textBoxName.TabIndex = 35;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(675, 423);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(117, 35);
+            this.saveButton.TabIndex = 55;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(534, 423);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(117, 35);
+            this.buttonCancel.TabIndex = 57;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // EditPersonalInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 477);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.comboBoxGender);
+            this.Controls.Add(this.dateTimePickerDOB);
+            this.Controls.Add(this.labelDateOfBirth);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.labelNationality);
+            this.Controls.Add(this.textBoxNation);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.labelContactNumber);
+            this.Controls.Add(this.textBoxContactNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.labelIdCard);
+            this.Controls.Add(this.textBoxIdCard);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.updateInformationLabel);
+            this.Name = "EditPersonalInformation";
+            this.Text = "EditPersonalInformation";
+            this.Load += new System.EventHandler(this.EditPersonalInformation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label updateInformationLabel;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDOB;
+        private System.Windows.Forms.Label labelDateOfBirth;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.Label labelNationality;
+        private System.Windows.Forms.TextBox textBoxNation;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label labelContactNumber;
+        private System.Windows.Forms.TextBox textBoxContactNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelIdCard;
+        private System.Windows.Forms.TextBox textBoxIdCard;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ Booking System/frontend/user/EditPersonalInformation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Booking_System.backend.model.user;
+
+namespace Booking_System.frontend.user
+{
+    public partial class EditPersonalInformation : Form
+    {
+        private readonly User user;
+        public EditPersonalInformation(User user)
+        {
+            InitializeComponent();
+            this.user = user;
+        }
+
+        private void EditPersonalInformation_Load(object sender, EventArgs e)
+        {
+            textBoxName.Text = user.FirstName;
+            textBoxSurname.Text = user.LastName;
+            textBoxEmail.Text = user.Email;
+            textBoxContactNumber.Text = user.ContactNumber;
+            dateTimePickerDOB.Value = user.DateOfBirth;
+            textBoxIdCard.Text = user.IdCard;
+
+            string gender = "";
+            switch (user.Gender)
+            {   
+                case 'M':
+                    gender = "Male";
+                    break;
+                case 'F':
+                    gender = "Female";
+                    break;
+                case 'O':
+                    gender = "Other";
+                    break;
+            }
+
+            comboBoxGender.Text = gender;
+            textBoxNation.Text = user.Nationality;
+            textBoxAddress.Text = user.Address;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
