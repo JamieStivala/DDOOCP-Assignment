ff36bf6ebca7be08533dacafa27985dc7c3e64c6
Jamie Stivala <jamiestivala@gmail.com>
2022-07-17 10:25:52 +0200

    Added GetFromDatabase which returns a tuple with Status -> Result.  Fixed naming conventions

--- Booking System/backend/database/DatabaseWrapper.cs
+++ Booking System/backend/database/DatabaseWrapper.cs
@@ -1,29 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Diagnostics;
 using System.Configuration;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Booking_System.backend.database
 {
     internal class DatabaseWrapper
     {
-        private static readonly string conString = ConfigurationManager.ConnectionStrings["Booking_System.Properties.Settings.dbConnectionString"].ConnectionString;//$"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={Application.StartupPath}\\bookings.mdb";
-        private static readonly OleDbConnection dbCon = new OleDbConnection(conString);
+        private static readonly string ConString = ConfigurationManager.ConnectionStrings["Booking_System.Properties.Settings.dbConnectionString"].ConnectionString;//$"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={Application.StartupPath}\\bookings.mdb";
+        private static readonly OleDbConnection DbCon = new OleDbConnection(ConString);
 
-        private static OleDbConnection getConnection()
+        private static OleDbConnection GetConnection()
         {
-            return dbCon;
+            return DbCon;
         }
 
         private static void Open()
         {
-            dbCon.Open();
+            DbCon.Open();
         }
 
         private static void Close()
         {
-            dbCon.Close();
+            DbCon.Close();
         }
 
         /**
@@ -32,12 +34,11 @@ namespace Booking_System.backend.database
         public static DatabaseResult InsertIntoDatabase(string query)
         {
             DatabaseResult result;
-            Debug.Write(DatabaseWrapper.conString);
 
             try
             {
                 DatabaseWrapper.Open();
-                OleDbCommand insertCommand = new OleDbCommand(query, DatabaseWrapper.getConnection());
+                OleDbCommand insertCommand = new OleDbCommand(query, DatabaseWrapper.GetConnection());
 
                 insertCommand.ExecuteNonQuery();
                 insertCommand.Dispose();
@@ -57,25 +58,66 @@ namespace Booking_System.backend.database
             return result;
         }
 
+        public static Tuple<DatabaseResult, Dictionary<string, object>>[] GetFromDatabase(string query)
+        {
+            List<Tuple<DatabaseResult, Dictionary<string, object>>> result = new List<Tuple<DatabaseResult, Dictionary<string, object>>>();
+
+            try
+            {
+                DatabaseWrapper.Open();
+                OleDbCommand selectCommand = new OleDbCommand(query, DatabaseWrapper.GetConnection());
+
+                using (OleDbDataReader reader = selectCommand.ExecuteReader())
+                {
+                    if (!reader.HasRows) result.Add(new Tuple<DatabaseResult, Dictionary<string, object>>(DatabaseResult.NotFound, null));
+                    else
+                    {
+                        while (reader.Read())
+                        {
+                            //The result is converted into a dictionary so it is independent of the OleDbReader.  This frees up memory and gives us the ability to close the connection and the reader before the data is manupulated
+                            Dictionary<string, object> currentRow = Enumerable.Range(0, reader.FieldCount).ToDictionary(reader.GetName, reader.GetValue); //Convert the current row into a dictionary (rowName -> rowResult)
+                            result.Add(new Tuple<DatabaseResult, Dictionary<string, object>>(DatabaseResult.Ok, currentRow)); //Store the current row as dbResult -> dictionary
+                        }
+
+                    }
+                }
+
+                selectCommand.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Message: {ex.Message}\nHash Code: {ex.GetHashCode()}");
+                result.Add(new Tuple<DatabaseResult, Dictionary<string, object>>(ConvertResult(ex.GetHashCode()), null));
+            }
+            finally
+            {
+                DatabaseWrapper.Close();
+            }
+
+            
+            return result.ToArray();
+        }
+
         private static DatabaseResult ConvertResult(int error)
         {
             switch (error)
             {
                 case 0:
-                    return DatabaseResult.OK;
+                    return DatabaseResult.Ok;
                 case 958745:
                 case 49205706:
-                    return DatabaseResult.DUPLICATE;
+                    return DatabaseResult.Duplicate;
                 default:
-                    return DatabaseResult.UNKNOWN;
+                    return DatabaseResult.Unknown;
             }
         }
     }
 
     internal enum DatabaseResult
     {
-        OK = 200,
-        DUPLICATE = 409,
-        UNKNOWN = 400
+        Ok = 200,
+        Duplicate = 409,
+        NotFound=404,
+        Unknown = 400
     } 
 }
