840f6ac0bb3848b9e84d4abed84264b074deb23c
Jamie Stivala <jamiestivala@gmail.com>
2022-07-18 11:04:08 +0200

    Added constructors to Hotel (db and fe creation)

--- Booking System/backend/model/hotel/Hotel.cs
+++ Booking System/backend/model/hotel/Hotel.cs
@@ -4,6 +4,19 @@ namespace Booking_System.backend.model.hotel
 {
     public class Hotel
     {
+        public Hotel(int id)
+        {
+            Id = id;
+        }
+
+        public Hotel(string name, string location, DateTime defaultCheckInTime, DateTime defaultCheckOutTime)
+        {
+            Name = name;
+            Location = location;
+            DefaultCheckInTime = defaultCheckInTime;
+            DefaultCheckOutTime = defaultCheckOutTime;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
