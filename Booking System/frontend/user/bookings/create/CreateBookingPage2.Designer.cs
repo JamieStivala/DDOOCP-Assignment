fdbe994bef857f1ed2453fb3e26109abeacb7526
Jamie Stivala <jamiestivala@gmail.com>
2022-07-20 17:27:48 +0200

    CreateBookingPage2 sample page for flow

---
+++ Booking System/frontend/user/bookings/create/CreateBookingPage2.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Booking_System.frontend.user.bookings.create
+{
+    partial class CreateBookingPage2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenuLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainMenuLabel
+            // 
+            this.mainMenuLabel.AutoSize = true;
+            this.mainMenuLabel.Font = new System.Drawing.Font("Segoe UI", 30F);
+            this.mainMenuLabel.Location = new System.Drawing.Point(42, 19);
+            this.mainMenuLabel.Name = "mainMenuLabel";
+            this.mainMenuLabel.Size = new System.Drawing.Size(248, 54);
+            this.mainMenuLabel.TabIndex = 72;
+            this.mainMenuLabel.Text = "Book a room";
+            // 
+            // CreateBookingPage2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainMenuLabel);
+            this.Name = "CreateBookingPage2";
+            this.Text = "CreateBookingPage2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainMenuLabel;
+    }
+}
---
+++ Booking System/frontend/user/bookings/create/CreateBookingPage2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Booking_System.frontend.user.bookings.create
+{
+    public partial class CreateBookingPage2 : Form
+    {
+        public CreateBookingPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
