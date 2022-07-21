a304d5a6e7bbd94c9a792fb2ce7f1b07abe1e86a
Jamie Stivala <jamiestivala@gmail.com>
2022-07-21 14:32:03 +0200

    Startup script modifications

--- Booking System/Program.cs
+++ Booking System/Program.cs
@@ -26,13 +26,13 @@ namespace Booking_System
             try
             {
                 bool adminFound = UserWrapper.GetAllUsers().Any(user => user.Type == UserType.Admin); //Check if an admin is existing, if not, create the default admin.
-
-                if(!adminFound) UserWrapper.CreateUser(new User("Jamie", "Stivala", "jamstivala@gmail.com", "Pukka1234!", "77223883", new DateTime(2001, 09, 16, 1, 1, 1), 'M', "0368201L", "Malta", "48 Rhapsody", UserType.Customer));
-                UserWrapper.CreateUser(new User("Admin", "Account", "admin@admin.com", "SecurePassword123!", "00000000", new DateTime(2001, 09, 16, 1, 1, 1), 'O', "000000A", "British", "London Bridge", UserType.Admin));
+                
+                UserWrapper.CreateUser(new User("Jamie", "Stivala", "jamiestivala@gmail.com", "Pukka1234!", "77223883", new DateTime(2001, 09, 16, 1, 1, 1), 'M', "0368201L", "Malta", "48 Rhapsody", UserType.Customer));
+                if (!adminFound) UserWrapper.CreateUser(new User("Admin", "Account", "admin@admin.com", "SecurePassword123!", "00000000", new DateTime(2001, 09, 16, 1, 1, 1), 'O', "000000A", "British", "London Bridge", UserType.Admin));
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("The default admin user already exists.");
+                Debug.WriteLine("An admin user already exists.");
                 //MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             //Create a default admin user
