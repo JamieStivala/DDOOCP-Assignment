95be621b67aa35ddeef953ea4a55a8877a1ecb0f
Jamie Stivala <jamiestivala@gmail.com>
2022-07-21 14:31:31 +0200

    Added a proper startup script which creates a default admin and user account if not existant

--- Booking System/Program.cs
+++ Booking System/Program.cs
@@ -3,6 +3,7 @@ using Booking_System.backend.model.user;
 using Booking_System.frontend.user;
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Windows.Forms;
 using Booking_System.backend.database.hotel;
 using Booking_System.backend.helpers;
@@ -21,15 +22,24 @@ namespace Booking_System
         [STAThread]
         static void Main()
         {
-            //UserWrapper.CreateUser(new User("Jamie", "Stivala", "jamiestivala@gmail.com", "Pukka1234!", "77223883", new DateTime(2001,09,16, 1,1,1), 'M', "0368201L", "Malta", "48 Rhapsody", UserType.Customer));
-            //UserWrapper.CreateUser(new User("Jamie", "Stivala", "jamie.stivala@gmail.com", "Pukka1234!", "77223883", new DateTime(2001,09,16, 1,1,1), 'M', "0368201L", "Malta", "48 Rhapsody", UserType.Customer));
-            //UserWrapper.CreateUser(new User("Jamie", "Stivalaoa", "jam.stivala@gmail.com", "Pukka1234!", "77223883", new DateTime(2001,09,16, 1,1,1), 'M', "0368201L", "Malta", "48 Rhapsody", UserType.Admin));
-            //var user = UserWrapper.GetUser("jamie.stivala@gmail.com", "Pukka1234!");
+            //Create a default admin user
+            try
+            {
+                bool adminFound = UserWrapper.GetAllUsers().Any(user => user.Type == UserType.Admin); //Check if an admin is existing, if not, create the default admin.
+
+                if(!adminFound) UserWrapper.CreateUser(new User("Jamie", "Stivala", "jamstivala@gmail.com", "Pukka1234!", "77223883", new DateTime(2001, 09, 16, 1, 1, 1), 'M', "0368201L", "Malta", "48 Rhapsody", UserType.Customer));
+                UserWrapper.CreateUser(new User("Admin", "Account", "admin@admin.com", "SecurePassword123!", "00000000", new DateTime(2001, 09, 16, 1, 1, 1), 'O', "000000A", "British", "London Bridge", UserType.Admin));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("The default admin user already exists.");
+                //MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //Create a default admin user
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new MainUserWindow(user));
             Application.Run(new FormSignin());
-            //Application.Run(new CreateBookingPage1(user));
         }
     }
 }
