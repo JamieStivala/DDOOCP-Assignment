49c02bf21a0b3b9d6a2998dfc687cbd9c8b0a32d
Jamie Stivala <jamiestivala@gmail.com>
2022-07-21 19:04:04 +0200

    Added a sample review wrapper with the ability and implemented create review

--- Booking System/backend/database/hotel/ReviewWrapper.cs
+++ Booking System/backend/database/hotel/ReviewWrapper.cs
@@ -1,6 +1,42 @@
-﻿namespace Booking_System.backend.database.hotel
+﻿using System;
+using Booking_System.backend.model.hotel;
+
+namespace Booking_System.backend.database.hotel
 {
     public class ReviewWrapper
     {
+
+        public static void CreateReview(Review review)
+        {
+            string query = "INSERT INTO tblReview (BookingId, RoomId, Title, [Description]) VALUES" +
+                           $"({review.BookingId}, {review.RoomId}, {review.Title}, {review.Description})";
+
+            (DatabaseResult, int) result = DatabaseWrapper.InsertIntoDatabaseReturningId(query);
+
+            switch (result.Item1)
+            {
+                case DatabaseResult.Ok:
+                    review.Id = result.Item2;
+                    ReviewWrapper.AddToCache(review);
+                    return;
+                default:
+                    throw new Exception("An unknown error has occurred.");
+            }
+        }
+
+        public static Review[] GetBookingReviews(int[] bookingId)
+        {
+            return null;
+        }
+
+        public static Review[] GetRoomReviews(int roomId)
+        {
+            return null;
+        }
+
+        private static void AddToCache(Review review)
+        {
+
+        }
     }
 }
